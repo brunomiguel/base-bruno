a14084fb8d3a7e65fe9cde8ec10255346fb01d07
Bruno Miguel <brunoalexandremiguel@gmail.com>
2019-10-17 21:54:10 +0100

    improved bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,56 @@ groups=(
   "base-devel"
 )
 
-echo -e "Adding userrepository to /etc/pacman.conf..."
-echo -e '\n[userrepository]\nServer = https://userrepository.eu\nSigLevel = Optional TrustAll' | sudo tee -a /etc/pacman.conf
-echo "Done."
-sleep 2s
+userrepository() {
+	read -p "Continue adding userrepository repo (y/n)?" CONT
+	if [ "$CONT" = "y" ]; then
+		echo -e "Adding userrepository to /etc/pacman.conf..."
+		echo -e '\n[userrepository]\nServer = https://userrepository.eu\nSigLevel = Optional TrustAll' | sudo tee -a pacman.conf
+		echo "Done.\n"
+		sleep 2s
+	else
+		echo -e "Skipped an essential step. Aborting..."
+		exit 0
+	fi
+}
 
-echo -e "Userrepository enabled. Now refreshing mirrors and installing any updates available..."
-sudo pacman -Syyuv --noconfirm
-echo "Done."
-sleep 2s
+refresh() {
+	echo -e "Userrepository enabled. Now refreshing mirrors and installing any updates available..."
+	sudo pacman -Syyuv --noconfirm
+	echo "Done.\n"
+	sleep 2s
+}
 
-echo -e "Installing base groups..."
-sudo pacman -Syuv ${groups[@]}
-echo "Done."
-sleep 2s
+groupinstall() {
+	read -p "Continue adding required groups (y/n)?" CONT
+	if [ "$CONT" = "y" ]; then
+		echo -e "Installing base groups..."
+		sudo pacman -Syuv ${groups[@]}
+		echo "Done.\n"
+		sleep 2s
+	else
+		echo "Skipped..."
+	fi
+}
 
-echo -e "Building and installing metapackage..."
-makepkg -C -c -d -f -i
-echo "Done."
-sleep 2s
+metapackage() {
+	read -p "Continue building metapackage (y/n)?" CONT
+	if [ "$CONT" = "y" ]; then
+		echo -e "Building and installing metapackage..."
+		makepkg -C -c -d -f -i
+		echo "Done.\n"
+		sleep 2s
+	else
+		echo "Skipped..."
+	fi
+}
 
-echo -e "All done.\n"
+finish() {
+	echo -e "All done.\n"
+}
+
+userrepository
+refresh
+groupinstall
+metapackage
+finish
